6ffab0a29059e3b063562811dd1017b2f5f92d42
onikolas <nikolas@xps>
2018-04-02 21:55:57 +0300

    Grid can now be read from file. Added example grid.json

--- grid2img.go
+++ grid2img.go
@@ -1,56 +1,96 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"image"
 	"image/color"
 	"image/png"
+	"io/ioutil"
 	"os"
+	//	"strconv"
 )
 
 type Grid struct {
-	cells [][]int
+	Cells     [][]int
+	Tiles     map[int]Cell
+	CellWidth int
 }
 
-func InitGrid(x, y int) *Grid {
-	g := &Grid{}
-	g.cells = make([][]int, y)
-	for i, _ := range g.cells {
-		g.cells[i] = make([]int, x)
+// makes the int keys into string keys to satisfy json
+/*func (i cellTypeMap) MarshalJSON() ([]byte, error) {
+	x := make(map[string]CellPainter)
+	for k, v := range i {
+		x[strconv.FormatInt(int64(k), 10)] = v
 	}
+	return json.Marshal(x)
+}*/
+
+func InitGrid(x, y, cellWidth int) *Grid {
+	g := &Grid{}
+	g.CellWidth = cellWidth
+	g.Cells = make([][]int, y)
+	for i, _ := range g.Cells {
+		g.Cells[i] = make([]int, x)
+	}
+	g.Tiles = make(map[int]Cell)
 	return g
 }
 
-func GridToImage(g *Grid, cellWidth int) *image.RGBA {
-	if len(g.cells) == 0 {
+func GridToImage(g *Grid) *image.RGBA {
+	if len(g.Cells) == 0 {
 		return nil
 	}
-	width := len(g.cells[0]) * cellWidth
-	height := len(g.cells) * cellWidth
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	width := len(g.Cells[0])
+	height := len(g.Cells)
+	img := image.NewRGBA(image.Rect(0, 0, width*g.CellWidth, height*g.CellWidth))
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			img.SetRGBA(x, y, color.RGBA{0, 0, 0, 0})
-			if g.cells[y/cellWidth][x/cellWidth] == 1 {
-				img.SetRGBA(x, y, color.RGBA{100, 0, 100, 255})
+			v, ok := g.Tiles[g.Cells[y][x]]
+			if ok {
+				v.Paint(x, y, g.CellWidth, img)
 			}
 		}
 	}
 	return img
 }
 
+type Cell struct {
+	Color color.RGBA
+}
+
+func (a Cell) Paint(x, y, w int, img *image.RGBA) {
+	for i := y * w; i < (y+1)*w; i++ {
+		for j := x * w; j < (x+1)*w; j++ {
+			img.SetRGBA(j, i, a.Color)
+		}
+	}
+}
+
 func main() {
 
-	g := InitGrid(8, 4)
-	g.cells[1][1] = 1
-	g.cells[2][2] = 1
-	g.cells[3][3] = 1
+	var gridfile = flag.String("grid", "grid.json", "grid file to load")
+	var imagefile = flag.String("image", "img.png", "image file to write")
+	flag.Parse()
 
-	img := GridToImage(g, 10)
-
-	f, err := os.Create("img.png")
+	// read gridfile and unmarhal into grid object
+	dat, err := ioutil.ReadFile(*gridfile)
 	if err != nil {
+		panic("could not read grid file ")
+	}
+	var g Grid
+	if err := json.Unmarshal(dat, &g); err != nil {
 		panic(err)
+	}
+
+	// make the image
+	img := GridToImage(&g)
+
+	// save it
+	f, err := os.Create(*imagefile)
+	if err != nil {
+		panic("could not create image file")
 	}
 	defer f.Close()
 	png.Encode(f, img)
